333aa2fdc62b3facc0bcfa78d90ad08d87d65e3b
Nathaniel Penaranda <nathaniel121215@gmail.com>
2024-11-15 08:27:35 +0800

    Initial Push

---
+++ sony_garage/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold underline">
+          NATHANIEL PENARANDA
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
